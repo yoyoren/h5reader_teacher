74200efb831634964abef07587a4a975e4024071
renyuan <402611233@qq.com>
2014-10-16 21:15:46 +0800

    4.7

---
+++ static/script/pages/index.js
@@ -0,0 +1,14 @@
+$.get('/ajax/index',function(d){
+   new Vue({
+   	  el:'#app',
+   	  data :{
+   	  	  top:d.items[0].data.data,
+   	  	  hot:d.items[1].data.data,
+   	  	  recommend:d.items[2].data.data,
+   	  	  female:d.items[3].data.data,
+   	  	  male:d.items[4].data.data,
+   	  	  free:d.items[5].data.data,
+   	  	  topic:d.items[6].data.data
+   	  }
+   })
+},'json');
