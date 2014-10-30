f4fb4ceaca9a5237452677522042a58a6431dec1
renyuan <402611233@qq.com>
2014-10-30 21:11:47 +0800

    4.8 code

--- static/script/pages/index.js
+++ static/script/pages/index.js
@@ -8,7 +8,32 @@ $.get('/ajax/index',function(d){
    	  	  female:d.items[3].data.data,
    	  	  male:d.items[4].data.data,
    	  	  free:d.items[5].data.data,
-   	  	  topic:d.items[6].data.data
+   	  	  topic:d.items[6].data.data,
+   	  	  duration:0,
+   	  	  position:0,
+   	  	  header_position:0,
+   	  	  header_duration:0,
+   	  	  tab_1_class:'Swipe-tab__on',
+   	  	  tab_2_class:''
+   	  },
+   	  methods:{
+   	  	tabSwitch:function(pos){
+   	  		this.duration = 0.5;
+   	  		this.header_duration = 0.5;
+   	  		if(pos == 0){
+   	  			this.position = 0;
+   	  			this.header_position = 0;
+   	  			this.tab_1_class = "Swipe-tab__on";
+   	  			this.tab_2_class = "";
+   	  		}else{
+   	  			this.position = (-734);
+   	  			this.header_position = 277;
+   	  			this.tab_2_class = "Swipe-tab__on";
+   	  			this.tab_1_class = "";
+   	  		}
+
+
+   	  	}
    	  }
    })
 },'json');
